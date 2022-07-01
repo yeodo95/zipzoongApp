262303dd46e924ae192b393900473d605ceb9075
yeodo95 <yeodo95@gmail.com>
2022-07-01 23:35:32 +0900

    ADD PACKAGE device_info_plus
    기기 고유 ID 얻기 패키지 추가

--- zipzoongApp/lib/main.dart
+++ zipzoongApp/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:io'; //Platform 사용을 위한 패키지
+import 'package:flutter/services.dart'; //PlatformException 사용을 위한 패키지
+import 'package:device_info_plus/device_info_plus.dart'; // 디바이스 정보 사용 패키지
+
 import 'package:zipzoongapp/screens/bid_infomation/bid_infomation.dart';
 import 'package:zipzoongapp/screens/dashboard/dashboard.dart';
 import 'package:zipzoongapp/theme.dart';
@@ -12,11 +16,23 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  @override
+  void initState() {
+    super.initState();
+    // print("print mobileId: $getMobileId()");
+    getMobileId();
+  }
+
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
@@ -51,5 +67,32 @@ class MyApp extends StatelessWidget {
         // GetPage(name:"/second", page: ()=>SecondNamedPage(), transition: Transition.fadeIn),
       ],
     );
+  }
+
+  Future<String?> getMobileId() async {
+    String? id = '';
+    try {
+      // 플랫폼 확인
+      if (Platform.isAndroid) {
+        // 안드로이드의 경우 androidInfo를 이용
+        // 이때 await으로 데이터 받을때까지 대기를 해야함
+        final AndroidDeviceInfo androidData =
+            await deviceInfoPlugin.androidInfo;
+        // 전달 받은 변수의 멤버 변수인 androidId가 고유 id이다.
+        id = androidData.androidId;
+      } else if (Platform.isIOS) {
+        // iOS의 경우 iosInfo를 이용
+        // 이때 await으로 데이터 받을때까지 대기를 해야함
+        final IosDeviceInfo iosData = await deviceInfoPlugin.iosInfo;
+        // 전달 받은 변수의 멤버 변수인 identifierForVendor가 고유 id이다.
+        id = iosData.identifierForVendor;
+      }
+    } on PlatformException {
+      // 어떠한 원인으로 실패할 경우
+      id = '';
+    }
+    // id 한번 출력해보고
+    print('id: $id');
+    return id;
   }
 }
